14e5bf2cf68398fb1de869dc635e11308cd7989f
Reid Mason <maddogshain132@gmail.com>
2021-03-13 03:29:15 +0000

    Added some additional logging info

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import time
-from config import TIME_TO_RUN
-from sync_runner import Syncher
 
 import schedule
 
+from config import TIME_TO_RUN
+from sync_runner import Syncher
+
+logger = utils.create_logger(__name__)
 
 def run_sync():
+    logger.info("Starting Anilist sync")
     syncher = Syncher()
     syncher.start_sync()
+    logger.info("Anilist sync finished")
 
-
+logger.info("Plex Anilist sync started.")
+logger.info(f"Plex Anilist sync waiting for scheduled time ({TIME_TO_RUN}) to run...")
 schedule.every().day.at(TIME_TO_RUN).do(run_sync)
 
 while True:
