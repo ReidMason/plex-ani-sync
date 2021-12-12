3a9de4bf805996ca5362171bb5889d3eb62fcfd4
Reid Mason <maddogshain132@gmail.com>
2021-12-12 18:44:47 +0000

    Neatened up season name display

--- server/models/plex/plexAnime.py
+++ server/models/plex/plexAnime.py
@@ -12,7 +12,7 @@ class PlexAnime:
         self.title: str = plex_season.parentTitle
         self.season_number: str = str(plex_season.seasonNumber)
         self.tvdb_id: Optional[int] = tvdb_id
-        self.display_name: str = f'{plex_season.parentTitle} - Season {self.season_number} ({self.tvdb_id})'
+        self.display_name: str = f'{plex_season.parentTitle}'
         self.release_year: Optional[int] = release_year
 
         self.plex_season: Season = plex_season
