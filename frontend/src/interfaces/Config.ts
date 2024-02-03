49c7de852fc37277860bf9bbaabd08b98b3b616d
Reid Mason <maddogshain132@gmail.com>
2024-02-03 23:30:42 +0000

    Added plex auth flow functions

--- frontend/src/interfaces/Config.ts
+++
@@ -1,7 +0,0 @@
-export default interface Config {
-    ANIME_LIBRARIES: Array<string>;
-    MARK_UNWATCHED_EPISODES_AS_PLANNING: boolean;
-    SYNC_CRONTIME: string;
-    SYNC_SCHEDULE_ENABLED: boolean;
-    DATE_FORMAT: string;
-}
