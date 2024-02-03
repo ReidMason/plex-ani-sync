60c1ecd837d7c1004e83f217fb621e240d8c5810
Reid Mason <maddogshain132@gmail.com>
2024-02-03 23:50:26 +0000

    Separated auth url generation and request

--- internal/plex/plex_test.go
+++ internal/plex/plex_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+func TestBuildAuthRequestUrl(t *testing.T) {
+	clientIdentifier := "testClientIdentifier"
+	appName := "testAppName"
+	expected := "https://plex.tv/api/v2/pins?X-Plex-Client-Identifier=testClientIdentifier&X-Plex-Product=testAppName&strong=true"
+
+	result := BuildAuthRequestUrl(clientIdentifier, appName)
+
+	if result != expected {
+		t.Errorf("BuildAuthRequestUrl(%s, %s) = %s; want %s", clientIdentifier, appName, result, expected)
+		fmt.Printf("result  : %v\n", result)
+		fmt.Printf("expected: %v\n", expected)
+	}
+}
+
 func TestCreateForwardUrl(t *testing.T) {
 	code := "testCode"
 	clientIdentifier := "testClientIdentifier"
