0275b696ef493835b6b6a08b46046b18ecc94c88
Reid Mason <maddogshain132@gmail.com>
2024-02-04 23:41:20 +0000

    Added unit test for connection string builder

---
+++ internal/db/dbmigrations_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBuiildConnectionString(t *testing.T) {
+	testCases := []struct {
+		username string
+		password string
+		host     string
+		port     string
+		dbName   string
+		expected string
+	}{
+		{"testuser1", "testpass1", "localhost", "5432", "plexAnilistSync", "postgres://testuser1:testpass1@localhost:5432/plexAnilistSync?sslmode=disable"},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("BuildConnectionString(%s, %s, %s, %s, %s)", tc.username, tc.password, tc.host, tc.port, tc.dbName), func(t *testing.T) {
+			t.Parallel()
+
+			result := BuildConnectionString(tc.username, tc.password, tc.host, tc.port, tc.dbName)
+
+			if result != tc.expected {
+				t.Errorf("BuildConnectionString(%s, %s, %s, %s, %s) = %s; want %s", tc.username, tc.password, tc.host, tc.port, tc.dbName, result, tc.expected)
+				fmt.Printf("result  : %v\n", result)
+				fmt.Printf("expected: %v\n", tc.expected)
+			}
+		})
+	}
+}
