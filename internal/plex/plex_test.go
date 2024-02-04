976c9024d4ae08b10806ba2b31736799a3416977
Reid Mason <maddogshain132@gmail.com>
2024-02-04 00:37:27 +0000

    Improved unit test structure

--- internal/plex/plex_test.go
+++ internal/plex/plex_test.go
@@ -6,56 +6,92 @@ import (
 )
 
 func TestBuildAuthRequestUrl(t *testing.T) {
-	clientIdentifier := "testClientIdentifier"
-	appName := "testAppName"
-	expected := "https://plex.tv/api/v2/pins?X-Plex-Client-Identifier=testClientIdentifier&X-Plex-Product=testAppName&strong=true"
-
-	result, err := BuildAuthRequestUrl(clientIdentifier, appName)
-	if err != nil {
-		t.Errorf("BuildAuthRequestUrl(%s, %s) threw an error", clientIdentifier, appName)
+	testCases := []struct {
+		clientIdentifier string
+		appName          string
+		expected         string
+	}{
+		{"testClientIdentifier1", "testAppName1", "https://plex.tv/api/v2/pins?X-Plex-Client-Identifier=testClientIdentifier1&X-Plex-Product=testAppName1&strong=true"},
+		{"testClientIdentifier2", "testAppName2", "https://plex.tv/api/v2/pins?X-Plex-Client-Identifier=testClientIdentifier2&X-Plex-Product=testAppName2&strong=true"},
 	}
 
-	if result != expected {
-		t.Errorf("BuildAuthRequestUrl(%s, %s) = %s; want %s", clientIdentifier, appName, result, expected)
-		fmt.Printf("result  : %v\n", result)
-		fmt.Printf("expected: %v\n", expected)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("BuildAuthRequestUrl(%s, %s)", tc.clientIdentifier, tc.appName), func(t *testing.T) {
+			t.Parallel()
+
+			result, err := BuildAuthRequestUrl(tc.clientIdentifier, tc.appName)
+			if err != nil {
+				t.Errorf("BuildAuthRequestUrl(%s, %s) threw an error", tc.clientIdentifier, tc.appName)
+			}
+
+			if result != tc.expected {
+				t.Errorf("BuildAuthRequestUrl(%s, %s) = %s; want %s", tc.clientIdentifier, tc.appName, result, tc.expected)
+				fmt.Printf("result  : %v\n", result)
+				fmt.Printf("expected: %v\n", tc.expected)
+			}
+		})
 	}
 }
 
-func TestCreateForwardUrl(t *testing.T) {
-	code := "testCode"
-	clientIdentifier := "testClientIdentifier"
-	appName := "testAppName"
-	expected := "https://app.plex.tv/auth/#?clientID=" + clientIdentifier + "&code=" + code + "&context%5Bdevice%5D%5Bproduct%5D=" + appName
-
-	result, err := BuildAuthUrl(code, clientIdentifier, appName)
-
-	if err != nil {
-		t.Errorf("BuildAuthUrl(%s, %s, %s) threw an error", code, clientIdentifier, appName)
+func TestBuildAuthUrl(t *testing.T) {
+	testCases := []struct {
+		code             string
+		clientIdentifier string
+		appName          string
+		expected         string
+	}{
+		{"testCode1", "testClientIdentifier1", "testAppName1", "https://app.plex.tv/auth/#?clientID=testClientIdentifier1&code=testCode1&context%5Bdevice%5D%5Bproduct%5D=testAppName1"},
+		{"testCode2", "testClientIdentifier2", "testAppName2", "https://app.plex.tv/auth/#?clientID=testClientIdentifier2&code=testCode2&context%5Bdevice%5D%5Bproduct%5D=testAppName2"},
 	}
 
-	if result != expected {
-		t.Errorf("BuildAuthUrl(%s, %s, %s) = %s; want %s", code, clientIdentifier, appName, result, expected)
-		fmt.Printf("result  : %v\n", result)
-		fmt.Printf("expected: %v\n", expected)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("BuildAuthUrl(%s, %s, %s)", tc.code, tc.clientIdentifier, tc.appName), func(t *testing.T) {
+			t.Parallel()
+
+			result, err := BuildAuthUrl(tc.code, tc.clientIdentifier, tc.appName)
+
+			if err != nil {
+				t.Errorf("BuildAuthUrl(%s, %s, %s) threw an error", tc.code, tc.clientIdentifier, tc.appName)
+			}
+
+			if result != tc.expected {
+				t.Errorf("BuildAuthUrl(%s, %s, %s) = %s; want %s", tc.code, tc.clientIdentifier, tc.appName, result, tc.expected)
+				fmt.Printf("result  : %v\n", result)
+				fmt.Printf("expected: %v\n", tc.expected)
+			}
+		})
 	}
 }
 
 func TestBuildPollingLink(t *testing.T) {
-	pinId := int64(123456)
-	pinCode := "testCode"
-	clientIdentifier := "testClientIdentifier"
-	expected := "https://plex.tv/api/v2/pins/123456?X-Plex-Client-Identifier=testClientIdentifier&code=testCode"
-
-	result, err := BuildPollingLink(pinId, pinCode, clientIdentifier)
-
-	if err != nil {
-		t.Errorf("BuildPollingLink(%d, %s, %s) threw an error", pinId, pinCode, clientIdentifier)
+	testCases := []struct {
+		pinCode          string
+		clientIdentifier string
+		expected         string
+		pinId            int64
+	}{
+		{"testCode1", "testClientIdentifier1", "https://plex.tv/api/v2/pins/123456?X-Plex-Client-Identifier=testClientIdentifier1&code=testCode1", 123456},
+		{"testCode2", "testClientIdentifier2", "https://plex.tv/api/v2/pins/123456?X-Plex-Client-Identifier=testClientIdentifier2&code=testCode2", 123456},
 	}
 
-	if result != expected {
-		t.Errorf("BuildPollingLink(%d, %s, %s) = %s; want %s", pinId, pinCode, clientIdentifier, result, expected)
-		fmt.Printf("result  : %v\n", result)
-		fmt.Printf("expected: %v\n", expected)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("BuildPollingLink(%d, %s, %s)", tc.pinId, tc.pinCode, tc.clientIdentifier), func(t *testing.T) {
+			t.Parallel()
+
+			result, err := BuildPollingLink(tc.pinId, tc.pinCode, tc.clientIdentifier)
+
+			if err != nil {
+				t.Errorf("BuildPollingLink(%d, %s, %s) threw an error", tc.pinId, tc.pinCode, tc.clientIdentifier)
+			}
+
+			if result != tc.expected {
+				t.Errorf("BuildPollingLink(%d, %s, %s) = %s; want %s", tc.pinId, tc.pinCode, tc.clientIdentifier, result, tc.expected)
+				fmt.Printf("result  : %v\n", result)
+				fmt.Printf("expected: %v\n", tc.expected)
+			}
+		})
 	}
 }
