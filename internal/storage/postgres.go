c4976e7639dd04ce15c197f59539a8cd185cbf8a
Reid Mason <maddogshain132@gmail.com>
2024-02-16 19:39:04 +0000

    Restructured internal application logic

---
+++ internal/storage/postgres.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"context"
+
+	storage "github.com/ReidMason/plex-ani-sync/internal/storage/postgres"
+)
+
+type Postgres struct {
+	queries *storage.Queries
+}
+
+func NewPostgresStorage(queries *storage.Queries) Postgres {
+	return Postgres{queries: queries}
+}
+
+func (p Postgres) GetUserById(id int32) (storage.User, error) {
+	ctx := context.Background()
+	return p.queries.GetUserById(ctx, id)
+}
+
+func (p Postgres) CreateUser(newUser storage.User) (storage.User, error) {
+	ctx := context.Background()
+	return p.queries.CreateUser(ctx, storage.CreateUserParams{
+		Name:      newUser.Name,
+		PlexToken: newUser.PlexToken,
+	})
+}
+
+func (p Postgres) UpdateUser(user storage.User) (storage.User, error) {
+	ctx := context.Background()
+	obj := storage.UpdateUserParams{
+		ID:        user.ID,
+		Name:      user.Name,
+		PlexToken: user.PlexToken,
+	}
+	return p.queries.UpdateUser(ctx, obj)
+}
---
+++ internal/storage/storage.go
@@ -0,0 +1,17 @@
+package storage
+
+import (
+	storage "github.com/ReidMason/plex-ani-sync/internal/storage/postgres"
+)
+
+type Storage interface {
+	GetUserById(id int32) (storage.User, error)
+	CreateUser(newUser storage.User) (storage.User, error)
+	UpdateUser(user storage.User) (storage.User, error)
+}
+
+type UpdateUserParams struct {
+	Name      string
+	PlexToken string
+	Id        int32
+}
