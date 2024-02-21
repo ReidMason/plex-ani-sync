dfa09026211bb431650e8fa8354a5ba6e2eacbcb
Reid Mason <maddogshain132@gmail.com>
2024-02-21 19:02:18 +0000

    Updated client_identifier field in database

--- db/migrations/000001_create_config_table.sql.up.sql
+++ db/migrations/000001_create_config_table.sql.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL UNIQUE,
   plex_token TEXT,
+  client_identifier TEXT NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
