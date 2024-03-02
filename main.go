c407fe049a603bb80685a8d33639e0a2c817b765
Reid Mason <maddogshain132@gmail.com>
2024-03-02 21:35:47 +0000

    Changed crednetials for storage config

--- main.go
+++ main.go
@@ -10,11 +10,17 @@ import (
 
 func main() {
 	listenAddr := flag.String("listen-addr", ":8000", "server listen address")
+	dbUser := flag.String("db-user", "admin", "database user")
+	dbPass := flag.String("db-pass", "admin", "database password")
+	dbHost := flag.String("db-host", "localhost", "database host")
+	dbPort := flag.String("db-port", "5432", "database port")
+	dbName := flag.String("db-name", "plexanilistsync", "database name")
 	flag.Parse()
 
-	storage, err := storage.NewPostgresStorage("testuser", "testpass", "localhost", "5432", "plexAnilistSync")
+	storage, err := storage.NewPostgresStorage(*dbUser, *dbPass, *dbHost, *dbPort, *dbName)
 	if err != nil {
 		log.Fatalf("Failed to initialise storage: %v", err)
+		panic(err)
 	}
 
 	server := api.NewServer(*listenAddr, storage)
