da09313c8c31fa93824e872364d4491c8e1b0709
Reid Mason <maddogshain132@gmail.com>
2024-03-03 21:31:47 +0000

    Created interface for media hosts

---
+++ internal/mediaHost/mediaHost.go
@@ -0,0 +1,13 @@
+package mediaHost
+
+import "net/http"
+
+type MediaHost interface {
+	Initialize(token string, host string, client HttpClient) error
+	GetLibraries() ([]Library, error)
+	GetCurrentUser() (PlexUser, error)
+}
+
+type HttpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
