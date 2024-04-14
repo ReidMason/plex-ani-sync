243f8bb8ec6ff903afd2778a7f4383ea8f42bbb4
Reid Mason <maddogshain132@gmail.com>
2024-04-14 22:57:07 +0100

    Created run function for program entrypoint

--- main.go
+++ main.go
@@ -2,38 +2,85 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
 	"os"
 
-	"github.com/ReidMason/plex-ani-sync/api"
+	"github.com/ReidMason/plex-ani-sync/internal/animeList"
+	"github.com/ReidMason/plex-ani-sync/internal/api"
 	"github.com/ReidMason/plex-ani-sync/internal/mediaHost"
 	"github.com/ReidMason/plex-ani-sync/internal/storage"
 	"github.com/ReidMason/plex-ani-sync/internal/userManager"
 	"github.com/charmbracelet/log"
 )
 
-func main() {
-	listenAddr := flag.String("listen-addr", ":8000", "server listen address")
-	dbUser := flag.String("db-user", "admin", "database user")
-	dbPass := flag.String("db-pass", "admin", "database password")
-	dbHost := flag.String("db-host", "localhost", "database host")
-	dbPort := flag.String("db-port", "5432", "database port")
-	dbName := flag.String("db-name", "plexanilistsync", "database name")
-	flag.Parse()
+type cmdArgs struct {
+	listenAddr string
+	dbUser     string
+	dbPass     string
+	dbHost     string
+	dbPort     string
+	dbName     string
+}
 
-	handler := log.New(os.Stdout)
+func run(w io.Writer, args cmdArgs) error {
+	handler := log.New(w)
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
 
-	storage, err := storage.NewPostgresStorage(*dbUser, *dbPass, *dbHost, *dbPort, *dbName)
+	storage, err := storage.NewPostgresStorage(args.dbUser, args.dbPass, args.dbHost, args.dbPort, args.dbName)
 	userManagerService := userManager.NewUserManager(storage)
 	if err != nil {
-		log.Fatalf("Failed to initialise storage: %v", err)
-		panic(err)
+		slog.Error("Failed to initialise storage", slog.Any("error", err))
+		return err
 	}
 
 	plex := mediaHost.NewPlex()
 
-	server := api.NewServer(*listenAddr, plex, userManagerService)
-	log.Fatal(server.Start())
+	client := http.Client{}
+	anilist := animeList.NewAnilist(&client, 392754)
+	animeList, err := anilist.GetAnimeList()
+	if err != nil {
+		slog.Error("Failed to get anime list", slog.Any("error", err))
+		return err
+	}
+
+	for _, entry := range animeList {
+		log.Infof("Anime ID: %s, Status: %s", entry.AnimeId, entry.Status)
+	}
+
+	results, err := anilist.SearchAnime("Naruto")
+	if err != nil {
+		slog.Error("Failed to search anime", slog.Any("error", err))
+		return err
+	}
+
+	slog.Info("Found anime", slog.Any("anime", results))
+
+	server := api.NewServer(args.listenAddr, plex, userManagerService)
+	if err := server.Start(); err != nil {
+		slog.Error("Failed to start server", slog.Any("error", err))
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	args := cmdArgs{
+		listenAddr: *flag.String("listen-addr", ":8000", "server listen address"),
+		dbUser:     *flag.String("db-user", "admin", "database user"),
+		dbPass:     *flag.String("db-pass", "admin", "database password"),
+		dbHost:     *flag.String("db-host", "localhost", "database host"),
+		dbPort:     *flag.String("db-port", "5432", "database port"),
+		dbName:     *flag.String("db-name", "plexanilistsync", "database name"),
+	}
+	flag.Parse()
+
+	if err := run(os.Stdout, args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
 }
