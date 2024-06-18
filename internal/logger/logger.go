390f5243f72b19a3c69e7e0d1698eb01f849207e
Reid Mason <maddogshain132@gmail.com>
2024-06-18 18:34:47 +0100

    feat: added debug log to interface

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,12 +3,17 @@ package logger
 import "github.com/charmbracelet/log"
 
 type Logger interface {
-	Info(msg string, keysAndValues ...interface{})
-	Error(msg string, keysAndValues ...interface{})
-	Warn(msg string, keysAndValues ...interface{})
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Error(msg string, args ...any)
+	Warn(msg string, args ...any)
 }
 
 type MockLogger struct{}
+
+func (m MockLogger) Debug(msg string, keysAndValues ...interface{}) {
+	log.Debug(msg, keysAndValues...)
+}
 
 func (m MockLogger) Info(msg string, keysAndValues ...interface{}) {
 	log.Info(msg, keysAndValues...)
