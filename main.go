9376d263f9f64282da9620dacb9d218f1fb9e745
Reid Mason <maddogshain132@gmail.com>
2024-06-18 18:34:25 +0100

    chore: added startup logs to entrypoint

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 	"github.com/ReidMason/plex-ani-sync/internal/mediaHost"
 	"github.com/ReidMason/plex-ani-sync/internal/request"
 	"github.com/ReidMason/plex-ani-sync/internal/storage"
-
-	synchandler "github.com/ReidMason/plex-ani-sync/internal/syncHandler"
 )
 
 const dbLocation = "data/data.db"
@@ -28,9 +26,14 @@ type cmdArgs struct {
 
 func run(w io.Writer, args cmdArgs) error {
 	handler := log.New(w)
+	handler.SetLevel(log.DebugLevel)
 	logger := slog.New(handler)
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	slog.SetDefault(logger)
 
+	logger.Debug("Starting up", slog.String("listenAddr", args.listenAddr))
+
+	logger.Info("Initialising storage")
 	storage, err := storage.NewSqliteStorage(dbLocation, logger)
 	if err != nil {
 		logger.Error("Failed to initialise storage", slog.Any("error", err))
@@ -42,46 +45,34 @@ func run(w io.Writer, args cmdArgs) error {
 	// 	logger.Error("Failed to reset database", slog.Any("error", err))
 	// }
 
+	logger.Info("Applying migrations")
 	err = storage.ApplyMigrations()
 	if err != nil {
 		logger.Error("Failed to apply migrations", slog.Any("error", err))
 		return err
 	}
 
+	logger.Info("Initialising media host")
 	plex := mediaHost.NewPlex()
 
+	logger.Info("Initialising animeList service")
 	client := request.NewStaggeredHttpClient(http.DefaultClient, logger)
 	anilist := animeList.NewAnilist(client, storage, logger)
 
-	server := api.NewServer(args.listenAddr, plex, anilist, storage, logger)
-	mediaHostService, err := server.InitialiseMediaHost()
+	logger.Info("Initialising mapping finder")
+	mappingFinder := mapping.NewMappingFinder(anilist, logger)
+
+	logger.Info("Initialising server")
+	server := api.NewServer(args.listenAddr, mappingFinder, plex, anilist, storage, logger)
+	logger.Info("Initialising setting up media host")
+	_, err = server.InitialiseMediaHost()
 	if err != nil {
 		logger.Error("Failed to initialise media host", slog.Any("error", err))
 	}
 
-	mappingFinder := mapping.NewMappingFinder(anilist, storage, mediaHostService, logger)
-	allSeries, err := mediaHostService.GetSeries("1")
-	if err != nil {
-		logger.Error("Failed to get series", slog.Any("error", err))
-	}
-	updateMappings(allSeries, mappingFinder, logger)
-
-	syncHandler := synchandler.NewSyncHandler(mediaHostService, storage, mappingFinder, anilist, logger)
-	syncHandler.Sync()
-
+	logger.Info("Starting server")
 	server.Start()
 	return nil
-}
-
-func updateMappings(allSeries []mediaHost.Series, mappingFinder mapping.MappingFinder, log *slog.Logger) {
-	log.Info("Updating mappings")
-	for _, series := range allSeries {
-		log.Info("Updating mappings", slog.String("title", series.Title))
-		err := mappingFinder.CreateMappings(series)
-		if err != nil {
-			log.Error("Failed to update mappings", slog.Any("error", err))
-		}
-	}
 }
 
 func main() {
